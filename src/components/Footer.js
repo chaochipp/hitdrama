b6ecce2c1f2308a593bd69ac6aa9f1747aff3358
bao-appscore <32353681+bao-appscore@users.noreply.github.com>
2023-10-01 21:35:06 +1100

    remove other things

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,12 +68,10 @@ export function Footer({ withSignUpForm = true, settings }) {
     <Bounded as="footer">
       <div className="grid grid-cols-1 justify-items-center gap-24">
         <HorizontalDivider />
-        {withSignUpForm && <SignUpForm settings={settings} />}
+        {/* {withSignUpForm && <SignUpForm settings={settings} />} */}
         <div className="mx-auto w-full max-w-3xl text-center text-xs font-semibold tracking-tight text-slate-500">
           Proudly published using{" "}
-          <Link href="https://prismic.io" className="text-slate-700">
-            Prismic
-          </Link>
+          Brain For Drama
         </div>
       </div>
     </Bounded>
